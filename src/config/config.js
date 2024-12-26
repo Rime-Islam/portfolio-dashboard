98b09e15e906e2ee701af8aa753322f80cc24552
rime-islam <rimeislam672@gmail.com>
2024-12-26 12:43:18 +0600

    image hook setup

---
+++ src/config/config.js
@@ -0,0 +1,6 @@
+const config = {
+    backendApi: 'http://localhost:5000/api',
+    cloudinary_preset: 'glamoraimage',
+    cloudinary_cloudname: 'dve5juerj',
+  };
+  export default config;
---
+++ src/lib/axios.js
@@ -0,0 +1,39 @@
+import config from "@/config/config";
+import { logout } from "@/service/auth";
+import axios from "axios";
+import Cookies from 'js-cookie';
+
+
+const axiosInstance = axios.create({
+  baseURL: config.backendApi,
+});
+
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const accessToken = Cookies.get("accessToken");
+    if (accessToken) {
+      config.headers = config.headers || {}; // Ensure headers object exists
+      config.headers.Authorization = `${accessToken}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error) 
+);
+
+
+axiosInstance.interceptors.response.use(
+  (response) => response, 
+  async (error) => {
+    if (error.response?.status === 401) {
+      try {
+        await logout(); 
+      } catch (logoutError) {
+        console.error("Error during logout:", logoutError);
+      }
+    }
+    return Promise.reject(error); 
+  }
+);
+
+export default axiosInstance;
